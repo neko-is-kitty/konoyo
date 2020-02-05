f316bab4f4eb134162353ca3a339f878711e9c18
K <7ka@criptext.com>
2020-02-05 13:49:22 -0700

    init
    Version 0.0.0 complete

--- main.js
+++ main.js
@@ -1,45 +1,84 @@
-const crypto = require('crypto');
-const defaults = require('dat-swarm-defaults');
-const readline = require('readline');
-const Swarm = require('discovery-swarm');
+const crypto = require('crypto')
+const hyperswarm = require('hyperswarm')
+const readline = require('readline')
 
-let rl
+// Init swarm
+const swarm = hyperswarm()
 
-// Sending messages
-const askUser = async () => {
-    rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout
-    })
-  
-    rl.question('Send message: ', message => {
-      // Broadcast to peers
-      for (let id in peers) {
-        peers[id].con.write(message)
-      }
-      rl.close()
-      rl = undefined
-      askUser()
-    });
-  }
-  
+console.log('Hello.')
 
-// ID
-const ID = `AM${crypto.randomBytes(32).toString('hex')}`
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: '->'
+})
 
-// Connect to peer
-const sw = Swarm(defaults({id: ID}));
+// Username variable
+var username = () => {
+  rl.setPrompt('@')
+  rl.question('What shall your username be?', (answer) => {
+    return answer
+  })
 
-async () => {
-    sw.listen()
-
-    sw.join('general')
-
-    sw.on('connection', function(con, info) {
-        console.log('Connected!')
-
-        con.on('data', data => {
-            console.log(`${info.peerId} — ${data.toString()}`)
-        })
-    })
+  rl.clearLine()
 }
+
+const topic = () => {
+  var buffer = crypto.createHash('sha256')
+    .update('main')
+    .digest()
+
+  username()
+  console.log('Waiting for peers.')
+  return buffer
+}
+
+// Join and connect
+swarm.join(topic(), {
+  lookup: true,
+  announce: true,
+  queue: { forget: 300000 }
+}, () => { console.log('Joined successfully! Waiting for connection.') })
+
+const sd = () => {
+  swarm.on('peer', (candidate) => {
+    // Info on peer
+    console.log(`Found a peer: (${candidate.host}, N° ${candidate.port}. Want to connect?)`)
+    rl.resume()
+    rl.on('line', (input) => {
+      const answer = Array.from(input)
+
+      switch (answer) {
+        case answer.includes('Y' || 'Yes' || 'yes'):
+          swarm.connect(candidate, (err, sock, meta) => {
+            console.log('Connected.')
+            if (err) { Error(err) }
+          })
+          break
+        case answer.includes('N' || 'No' || 'no'):
+          swarm.connect(candidate, (err, sock, meta) => {
+            meta.backoff()
+            console.log('Peer rejected.')
+            if (err) { Error(err) }
+          })
+          break
+      }
+    })
+  })
+
+  swarm.on('connection', (sock, meta) => {
+    sock.on('data', (data) => {
+      rl.pause()
+      console.log(data)
+      rl.resume()
+    })
+
+    rl.on('line', (line) => {
+      rl.pause()
+      sock.write(line)
+      rl.resume()
+    })
+  })
+}
+
+sd()
